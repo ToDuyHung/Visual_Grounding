3dac1c2b61a6d2d2358917cb04f89bb366c67ce7
ToDuyHung <hung.toduy166@gmail.com>
2022-11-08 17:51:23 +0700

    add inference one image

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
-# Visual_Grounding
-test test 2
+# Visual-Grounding
+
+A project based on an unified sequence-to-sequence pretrained model called OFA to finetune the Visual Grounding task.
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/vg.jpg" />
+</p>
+<p align="center">
+  <b>Figure 1:</b> Visual Grounding task
+</p>
+
+## Table of contents
+* [Requirements](#requirements)
+* [Setup_environment](#setup_environment)
+* [Checkpoints](#checkpoints)
+* [Inference](#inference)
+* [VGApi](#vgapi)
+
+## Requirements
+
+- python 3.7.4
+- pytorch 1.8.1
+- torchvision 0.9.1
+
+## Setup_environment
+
+1. Install ofa environment by running the following command:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+2. Install fairseq by running the following command:
+
+     ```
+     cd fairseq
+     pip install .
+     ```
+
+## Checkpoints
+Checkpoints, which include utils and pretrained model, used in this project is stored in this google drive link: https://drive.google.com/file/d/15DdejkzFOzGUpmeqO_lyEWhfobkFzXxd/view?usp=sharing
+
+* model/utils/BPE: byte pair encoding
+* model/checkpoints/visual_grounding: pretrained visual grounding checkpoint based on ofa_base.pt
+
+## Inference
+    
+    cd testVG
+    python processor.py
+
+## VGApi
+    python app_dev.py
+
+Some demo image:
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/black dress (2).jpg" />
+</p>
+<p align="center">
+  <b>Figure 2:</b> Black dress
+</p>
+
+<p align="center">
+  <img style="width:30%" align="center" src="demo_image/black glass.jpg" />
+</p>
+<p align="center">
+  <b>Figure 3:</b> Black glass
+</p>
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/flower pant.jpg" />
+</p>
+<p align="center">
+  <b>Figure 4:</b> Flower pant
+</p>
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/long sleeve.jpg" />
+</p>
+<p align="center">
+  <b>Figure 5:</b> Long sleeve
+</p>
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/man's hat.jpg" />
+</p>
+<p align="center">
+  <b>Figure 6:</b> Man's hat
+</p>
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/woman's shirt.jpg" />
+</p>
+<p align="center">
+  <b>Figure 7:</b> Woman's shirt
+</p>
+
+<p align="center">
+  <img style="width:50%" align="center" src="demo_image/messi shirt.jpg" />
+</p>
+<p align="center">
+  <b>Figure 8:</b> Messi shirt
+</p>
+
+<p align="center">
+  <img style="width:30%" align="center" src="demo_image/middle shirt.jpg" />
+</p>
+<p align="center">
+  <b>Figure 9:</b> Middle shirt
+</p>
+
+<p align="center">
+  <img style="width:40%" align="center" src="demo_image/bottom right shirt.jpg" />
+</p>
+<p align="center">
+  <b>Figure 10:</b> Bottom right shirt
+</p>
---
+++ testVG/processor.py
@@ -0,0 +1,160 @@
+import torch
+import cv2
+import numpy as np
+from fairseq import utils, tasks
+from fairseq import checkpoint_utils
+from services.utils.eval_utils import eval_step
+from services.tasks.mm_tasks.refcoco import RefcocoTask
+from services.models.ofa import OFAModel
+from PIL import Image, ImageDraw, ImageFont
+from io import BytesIO
+import base64
+import requests
+import re
+import pickle
+from tqdm import tqdm
+from glob import glob
+
+# Register refcoco task
+tasks.register_task('refcoco', RefcocoTask)
+# tasks.register_task('vqagen', VqaGenTask)
+
+# turn on cuda if GPU is available
+use_cuda = torch.cuda.is_available()
+# use fp16 only when GPU is available
+use_fp16 = False
+
+# Load pretrained ckpt & config
+overrides={"bpe_dir":"model/utils/BPE"}
+models, cfg, task = checkpoint_utils.load_model_ensemble_and_task(
+        utils.split_paths('model/checkpoints/visual_grounding/refcoco_base_best.pt'),
+        arg_overrides=overrides
+    )
+
+cfg.common.seed = 7
+cfg.generation.beam = 5
+cfg.generation.min_len = 4
+cfg.generation.max_len_a = 0
+cfg.generation.max_len_b = 4
+cfg.generation.no_repeat_ngram_size = 3
+
+# Fix seed for stochastic decoding
+if cfg.common.seed is not None and not cfg.generation.no_seed_provided:
+    np.random.seed(cfg.common.seed)
+    utils.set_torch_seed(cfg.common.seed)
+
+# Move models to GPU
+for model in models:
+    model.eval()
+    if use_fp16:
+        model.half()
+    if use_cuda and not cfg.distributed_training.pipeline_model_parallel:
+        model.cuda()
+    model.prepare_for_inference_(cfg)
+
+# Initialize generator
+generator = task.build_generator(models, cfg.generation)
+
+# Image transform
+from torchvision import transforms
+mean = [0.5, 0.5, 0.5]
+std = [0.5, 0.5, 0.5]
+
+patch_resize_transform = transforms.Compose([
+    lambda image: image.convert("RGB"),
+    transforms.Resize((cfg.task.patch_image_size, cfg.task.patch_image_size), interpolation=Image.BICUBIC),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
+])
+
+# Text preprocess
+bos_item = torch.LongTensor([task.src_dict.bos()])
+eos_item = torch.LongTensor([task.src_dict.eos()])
+pad_idx = task.src_dict.pad()
+def encode_text(text, length=None, append_bos=False, append_eos=False):
+    s = task.tgt_dict.encode_line(
+        line=task.bpe.encode(text.lower()),
+        add_if_not_exist=False,
+        append_eos=False
+    ).long()
+    if length is not None:
+        s = s[:length]
+    if append_bos:
+        s = torch.cat([bos_item, s])
+    if append_eos:
+        s = torch.cat([s, eos_item])
+    return s
+
+# Construct input for refcoco task
+patch_image_size = cfg.task.patch_image_size
+def construct_sample(image: Image, text: str):
+    w, h = image.size
+    w_resize_ratio = torch.tensor(patch_image_size / w).unsqueeze(0)
+    h_resize_ratio = torch.tensor(patch_image_size / h).unsqueeze(0)
+    patch_image = patch_resize_transform(image).unsqueeze(0)
+    patch_mask = torch.tensor([True])
+    src_text = encode_text(' which region does the text " {} " describe?'.format(text), append_bos=True, append_eos=True).unsqueeze(0)
+    src_length = torch.LongTensor([s.ne(pad_idx).long().sum() for s in src_text])
+    sample = {
+        "id":np.array(['42']),
+        "net_input": {
+            "src_tokens": src_text,
+            "src_lengths": src_length,
+            "patch_images": patch_image,
+            "patch_masks": patch_mask,
+        },
+        "w_resize_ratios": w_resize_ratio,
+        "h_resize_ratios": h_resize_ratio,
+        "region_coords": torch.randn(1, 4)
+    }
+    return sample
+  
+# Function to turn FP32 to FP16
+def apply_half(t):
+    if t.dtype is torch.float32:
+        return t.to(dtype=torch.half)
+    return t
+
+if __name__ == '__main__':
+    # font
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    
+    # org
+    org = (50, 50)
+    
+    # fontScale
+    fontScale = 1
+    
+    # Blue color in BGR
+    color = (255, 0, 0)
+    
+    # Line thickness of 2 px
+    thickness = 2
+    file = 'black bow shirt.jpg'
+    text = 'black bow shirt'
+    image = Image.open(file)
+    
+    # Construct input sample & preprocess for GPU if cuda available
+    sample = construct_sample(image, text)
+    sample = utils.move_to_cuda(sample) if use_cuda else sample
+    sample = utils.apply_to_sample(apply_half, sample) if use_fp16 else sample
+
+    # Run eval step for refcoco
+    with torch.no_grad():
+        result, scores = eval_step(task, generator, models, sample)
+        
+    # from google.colab.patches import cv2_imshow
+
+    img = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+    cv2.rectangle(
+        img,
+        (int(result[0]["box"][0]), int(result[0]["box"][1])),
+        (int(result[0]["box"][2]), int(result[0]["box"][3])),
+        (0, 255, 0),
+        3
+    )
+    
+    # Using cv2.putText() method
+    img = cv2.putText(img, text, org, font, 
+                    fontScale, color, thickness, cv2.LINE_AA)
+    cv2.imwrite(f'demo.jpg', img)
